c78501417ee2e3ac155e0cb95df375af9cf6ddd8
Vidya <vidya.pogiri@mediware.com>
2019-02-04 18:23:17 +0530

    Tweaked the code by adding By class for POM

---
+++ src/main/java/com/wikipedia/SearchPage.java
@@ -0,0 +1,32 @@
+package com.wikipedia;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class SearchPage extends TestBase {
+
+	public static By searchInputText = By.id("searchInput");
+	public static By searchButton = By.xpath("//button[@type='submit']");
+	
+	
+	// Initializing the Page Objects:
+	public SearchPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	public static void searchText(String text){
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(searchInputText).sendKeys(text);
+		driver.findElement(searchButton).click();
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/com/wikipedia/SearchWikipediaTest.java
@@ -0,0 +1,62 @@
+/*
+ * @author Naveen Khunteta
+ * 
+ */
+
+package com.wikipedia;
+
+
+import java.util.Random;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+import com.crm.qa.base.TestBase;
+
+
+public class SearchWikipediaTest extends TestBase{
+
+	   
+	public SearchWikipediaTest(){
+			super();
+			
+	}
+	
+	
+	@BeforeMethod
+	public void setUp() throws InterruptedException {	
+		initialization();
+	}
+	
+	/*@DataProvider
+	public Object[][] getCRMTestData(){
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}*/
+	
+	
+	@Test()
+	public void SearchWikipedia(){
+		String[] keyWords = {"Marvels","Superman","Ironman","Batman","Wonderwomen","Captain America","Avengers"};
+		int id = new Random().nextInt(keyWords.length);
+		String random = (keyWords[id]);
+		System.out.println(random);
+		com.wikipedia.SearchPage.searchText(random);
+	}
+	
+
+	  
+
+
+
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+}
