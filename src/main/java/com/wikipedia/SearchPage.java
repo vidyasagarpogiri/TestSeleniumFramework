99f05be1dcd6bfdac065bd85f8ac30560a76001f
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-11 00:11:30 +0530

    Included import com.crm.qa.base.TestBase;
    Included import com.crm.qa.base.TestBase;

--- src/main/java/com/wikipedia/SearchPage.java
+++ src/main/java/com/wikipedia/SearchPage.java
@@ -3,11 +3,11 @@ package com.wikipedia;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.PageFactory;    
 
 import com.crm.qa.base.TestBase;
 
-public class SearchPage extends TestBase {
+public class SearchPage extends TestBase {  
 
 	public static By searchInputText = By.id("searchInput");
 	public static By searchButton = By.xpath("//button[@type='submit']");
