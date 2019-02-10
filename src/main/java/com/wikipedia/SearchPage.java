e7d8a6b3758812dcd1e5965a246ec5bdc13c9f2d
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-11 00:12:48 +0530

    Included TestBase
    Included TestBase

--- src/main/java/com/wikipedia/SearchPage.java
+++ src/main/java/com/wikipedia/SearchPage.java
@@ -1,6 +1,6 @@
 package com.wikipedia;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;   
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;       
