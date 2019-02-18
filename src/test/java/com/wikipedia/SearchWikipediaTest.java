bfb59a797f23bf66e4a5cc120fa105c2f46d6487
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-18 14:49:19 +0530

    wikipedia
    wikipedia

--- src/test/java/com/wikipedia/SearchWikipediaTest.java
+++ src/test/java/com/wikipedia/SearchWikipediaTest.java
@@ -10,7 +10,7 @@ import java.util.Random;
 import org.testng.annotations.AfterMethod;              
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.Test;               
 
 
 import com.crm.qa.base.TestBase;
