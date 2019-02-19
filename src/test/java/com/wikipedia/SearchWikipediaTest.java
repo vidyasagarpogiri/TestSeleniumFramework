aee609a256c6f17ae91efc06b25fd06926a32837
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2019-02-19 14:01:06 +0530

    Added teardown
    Added teardown

--- src/test/java/com/wikipedia/SearchWikipediaTest.java
+++ src/test/java/com/wikipedia/SearchWikipediaTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;               
                 
-
+           
 import com.crm.qa.base.TestBase;
             
 
